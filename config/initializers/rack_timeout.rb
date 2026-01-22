fa0efe48ed3ce4cd45f9eb7f57f7c0b43c249041
agenciamindfulmarketing <agenciamindfulmarketing@gmail.com>
2026-01-22 16:51:15 -0300

    Update rack_timeout.rb
    ERA ASSIM 
    chatwoot/config/initializers/rack_timeout.rb
    
    
    require 'rack-timeout'
    
    # Reduce noise by filtering state=ready and state=completed which are logged at INFO level
    Rails.application.config.after_initialize do
      Rack::Timeout::Logger.level = Logger::ERROR
    end

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,6 +1,9 @@
 require 'rack-timeout'
 
-# Reduce noise by filtering state=ready and state=completed which are logged at INFO level
+# Tempo máximo da requisição (em segundos)
+Rack::Timeout.timeout = 60
+
+# Reduz ruído de log
 Rails.application.config.after_initialize do
   Rack::Timeout::Logger.level = Logger::ERROR
 end
